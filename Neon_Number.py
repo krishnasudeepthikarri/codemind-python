41d6fda168fd0e693a7da4d3984eb159ad615351
krishnasudeepthikarri <99941553+krishnasudeepthikarri@users.noreply.github.com>
2023-05-26 03:22:54 -0700

    n=int(input()) #9 a=n*n  #a=81 s=0 while a!=0:     rem=a%10 #1     a//=10     s+=rem  # if s==n:     print("Neon Number") else:     print("Not Neon Number")

--- Neon_Number.py
+++ Neon_Number.py
@@ -1,10 +1,10 @@
-n=int(input())
-a=n*n
+n=int(input()) #9
+a=n*n  #a=81
 s=0
-while a!=0:
-    rem=a%10
+while a!=0: 
+    rem=a%10 #1
     a//=10
-    s+=rem
+    s+=rem  #
 if s==n:
     print("Neon Number")
 else:
