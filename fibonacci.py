eb353510e823d461d190145a7e748b87338c6b9f
krishnasudeepthikarri <99941553+krishnasudeepthikarri@users.noreply.github.com>
2023-05-26 21:55:12 -0700

    n=int(input()) n1,n2=0,1 print(n1,n2,end=' ') for i in range(2,n):     n3=n1+n2     n1=n2     n2=n3     print(n3,end=' ')

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,8 @@
-n=int(input()) #10
-n1=0
-n2=1
-print(n1,n2,end=' ') #0,1
-for i in range(2,n): 
-    n3=n1+n2  #n3=0+1=1
-    n1=n2     #n1=1
-    n2=n3     #n2=1
+n=int(input())
+n1,n2=0,1
+print(n1,n2,end=' ')
+for i in range(2,n):
+    n3=n1+n2
+    n1=n2
+    n2=n3
     print(n3,end=' ')
