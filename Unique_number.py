eadc5bad32d1b86e402640b2b028b43f2cae1b78
krishnasudeepthikarri <99941553+krishnasudeepthikarri@users.noreply.github.com>
2023-05-26 21:50:27 -0700

    n=int(input()) l=[] i=0 while n!=0:     rem=n%10     n//=10     i+=1     l.append(rem)     a=set(l) if len(a)==i:     print("Unique Number") else:     print("Not Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,18 +1,13 @@
-def fun(d,num):
-    while num:
-        k=num%10
-        num=num//10
-        if d==k:
-            return True
-    return False
 n=int(input())
-s=0
-while n:
-    d=n%10
-    n=n//10
-    if fun(d,s):
-        print("Not Unique Number")
-        break
-    s=s*10+d
+l=[]
+i=0
+while n!=0:
+    rem=n%10
+    n//=10
+    i+=1
+    l.append(rem)
+    a=set(l)
+if len(a)==i:
+    print("Unique Number")
 else:
-        print("Unique Number")
+    print("Not Unique Number")
