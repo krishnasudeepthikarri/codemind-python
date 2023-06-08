d670ff07ffe517ac26c2d19735f1a2701927dcc6
krishnasudeepthikarri <99941553+krishnasudeepthikarri@users.noreply.github.com>
2023-06-08 11:34:36 +0530

    import math def isprime(n):     if n==1:         return False     sq=int(math.sqrt(n))     for i in range(2,sq+1):         if n%i==0:             return False     return True n=int(input()) if isprime(n):     while n:         d=n%10         n//=10         if d==1 or d==4 or d==6 or d==9:             print("Not Mega Prime")             break     else:         print("Mega Prime") else:     print("Not Mega Prime")

--- mega_prime.py
+++ mega_prime.py
@@ -1,20 +1,18 @@
 import math
-def isprime(num):
-    if num==1:
-        return False 
-    sq=int(math.sqrt(num))
+def isprime(n):
+    if n==1:
+        return False
+    sq=int(math.sqrt(n))
     for i in range(2,sq+1):
-        if num%i==0:
+        if n%i==0:
             return False
     return True
-num=int(input())
-if isprime(num):
-    while num:
-        d=num%10
-        num=num//10
-        if d==2 or d==3 or d==5 or d==7:
-            pass
-        else:
+n=int(input())
+if isprime(n):
+    while n:
+        d=n%10
+        n//=10
+        if d==1 or d==4 or d==6 or d==9:
             print("Not Mega Prime")
             break
     else:
