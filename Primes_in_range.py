eb633e422798eaf24556d4bdf571061d08f08072
krishnasudeepthikarri <99941553+krishnasudeepthikarri@users.noreply.github.com>
2023-06-08 10:25:19 +0530

    def isprime(n):     if(n==1):         return 0     else:         for i in range(2,int(n**0.5)+1):             if(n%i==0):                 return 0                 break     return 1 a=int(input()) b=int(input()) c=0 for i in range(a,b+1):     if(isprime(i)==1):         c+=1 print(c)

---
+++ Primes_in_range.py
@@ -0,0 +1,16 @@
+def isprime(n):
+    if(n==1):
+        return 0
+    else:
+        for i in range(2,int(n**0.5)+1):
+            if(n%i==0):
+                return 0
+                break
+    return 1
+a=int(input())
+b=int(input())
+c=0
+for i in range(a,b+1):
+    if(isprime(i)==1):
+        c+=1
+print(c)
