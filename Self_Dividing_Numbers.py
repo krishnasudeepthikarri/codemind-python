4f2f3202905511631963c371947ca0159d02f7eb
krishnasudeepthikarri <99941553+krishnasudeepthikarri@users.noreply.github.com>
2024-08-06 21:17:32 +0530

    def sd(num):     temp=num     while temp!=0:         rem=temp%10         temp=temp//10         if rem==0 or num%rem!=0:             return False     return True n1=int(input()) n2=int(input()) for i in range(n1,n2+1):     if sd(i):         print(i,end=" ")

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -2,12 +2,12 @@ def sd(num):
     temp=num
     while temp!=0:
         rem=temp%10
-        temp//=10
+        temp=temp//10
         if rem==0 or num%rem!=0:
             return False
     return True
-m=int(input())
-n=int(input())
-for i in range(m,n+1):
+n1=int(input())
+n2=int(input())
+for i in range(n1,n2+1):
     if sd(i):
         print(i,end=" ")
